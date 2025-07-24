d5262bdbbc4e4702f6a1994d73a4514745f1c890
muhammad uzair <abuzada735@gmail.com>
2025-07-24 22:13:00 +0500

    feat: add cookie-parser dependency; implement auth middleware for token verification and user retrieval

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import postsRouter from "./routes/posts.route.js";
+import cors from "cors";
 
 // initialize express app
 const app = express();
 
-// Middleware to parse JSON and URL-encoded data
+// Middleware to parse JSON, URL-encoded data and enable CORS
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
 
 // default home route to test the server
 app.get("/", (req, res) => {
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -54,7 +54,21 @@ userSchema.methods.generateAccessToken = function () {
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRATION,
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
   );
 };
